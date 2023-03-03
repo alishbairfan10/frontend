14eb19e8b264c37a92de1f9cdb22cab866d712d6
alishbairfan10 <alishba2209d@aptechgdn.net>
2023-03-02 20:52:31 -0800

    website

---
+++ app.js
@@ -0,0 +1,23 @@
+document.getElementById("btn1").addEventListener("click",function()
+{
+   $("#text").show();
+   $("#text1").hide();
+})
+ document.getElementById("btn2").addEventListener("click",function()
+ {
+    $("#text1").show();
+    $("#text").hide();
+})
+
+function fun(){
+ alert("Added to cart.");
+  
+}
+function fun1(){
+  
+    
+  }
+  function fun2(){
+   alert("Logged in.");
+    
+  }
